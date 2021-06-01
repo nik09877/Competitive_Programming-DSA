d98d8271ae4ce2ae618c7795a7162f223dbad4e0
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2021-06-01 18:09:50 +0530

    added new problems

--- binary_search_C_Maximum_Median.cpp
+++ binary_search_C_Maximum_Median.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include <stdio.h>
 #define rep(i, n) for (int i = 0; i < n; i++)
 #define rrep(i, a, b) for (int i = a; i >= b; i--)
 #define rep1(i, n) for (int i = 1; i <= n; i++)
@@ -36,31 +35,44 @@
 #define pr(x) cout << x
 #define prsp(x) cout << x << sp
 #define prln(x) cout << x << endl
-#define fastio ios_base::sync_with_stdio(0), cout.tie(0), cin.tie(0)
-#define read(a, n) \
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0)
+#define re(a, n)   \
     rep(i, n)      \
             cin >> \
         a[i];
 using namespace std;
 const int mod = 1000000007;
+#define int long long
 
-class Solution
+bool good(int m, vi &a, int &n, int &k)
 {
-public:
-    // Nim Game
-    bool canWinNim(int n)
+    int req = 0;
+    fo(i, n / 2, n - 1)
     {
-        return n % 4 != 0;
+        if (a[i] < m)
+            req += (m - a[i]);
     }
-};
+    return (req <= k);
+}
 int32_t main()
 {
     fastio;
-    int t = 1;
-    cin >> t;
-    while (t--)
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    re(a, n);
+    asort(a);
+    int l = a[n / 2], r = a[n - 1] + k + 1;
+    rep(i, 35)
     {
-        int n;
-        cin >> n;
+        int m = (l + r) / 2;
+        if (good(m, a, n, k))
+        {
+            l = m;
+        }
+        else
+            r = m;
     }
+    prln(l);
+    return 0;
 }
