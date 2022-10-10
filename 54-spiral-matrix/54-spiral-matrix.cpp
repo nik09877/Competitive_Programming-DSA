6da1ec2af659c5a4d6bc459fc3b3c13076d13641
Nikhil Kumar Mishra <54058009+nik09877@users.noreply.github.com>
2022-10-10 23:21:13 +0530

    Time: 3 ms (45.26%), Space: 6.8 MB (91.81%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -6,6 +6,19 @@ public:
         int pointer_to_fst_col = 0,pointer_to_lst_col=m-1;
         vector<int>ans;
         
+        /*
+        In each outer loop traversal print the elements of a square in a clockwise manner.
+
+        Print the top row, i.e. Print the elements of the top row from column index left to right and increase the count of the top so that it will move to the next row.
+
+        Print the right column, i.e. Print the rightmost column from row index top to bottom and decrease the count of right.
+
+        Print the bottom row, i.e. if top <= bottom, then print the elements of a bottom row from column right to left and decrease the count of bottom
+
+        Print the left column, i.e. if left <= right, then print the elements of the left column from the bottom row to the top row and increase the count of left.
+
+        Run a loop until all the squares of loops are printed.
+        */
         while(pointer_to_fst_row<=pointer_to_lst_row
              and
              pointer_to_fst_col<=pointer_to_lst_col){
