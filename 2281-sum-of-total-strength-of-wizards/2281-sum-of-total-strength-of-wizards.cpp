d281f0db8681cc440488904635d09245018a5524
Nikhil Kumar Mishra <54058009+nik09877@users.noreply.github.com>
2022-11-03 21:01:36 +0530

    Time: 526 ms (57.62%), Space: 105.6 MB (27.29%) - LeetHub

---
+++ 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+int totalStrength(vector<int>& st) {
+    long long MOD = 1'000'000'007;
+    const int N = st.size();
+    // sum of first k elements
+    vector<long long> prefix(N + 1, 0L);
+    for (int i = 0; i < N; ++i) {
+        prefix[i + 1] = (prefix[i] + st[i]) % MOD;
+    }
+    // sum of first k prefix
+    vector<long long> prefix_sum(N + 2, 0L);
+    for (int i = 0; i <= N; ++i) {
+        prefix_sum[i + 1] = (prefix_sum[i] + prefix[i]) % MOD;
+    }
+    
+    // first index on the left < current st
+    vector<int> left(N, -1);
+    // mono increase
+    vector<int> stack;
+    for (int i = 0; i < N; ++i) {
+        while (!stack.empty() && st[stack.back()] >= st[i]) {
+            stack.pop_back();
+        }
+        left[i] = stack.empty() ? -1 : stack.back();
+        stack.push_back(i);
+    }
+    
+    // first index on the right <= current st
+    vector<int> right(N, N);
+    stack.clear();
+    for (int i = N - 1; i >= 0; --i) {
+        while (!stack.empty() && st[stack.back()] > st[i]) {
+            stack.pop_back();
+        }
+        right[i] = stack.empty() ? N : stack.back();
+        stack.push_back(i);
+    }
+    
+    long long res = 0;
+    for (int i = 0; i < N; ++i) {
+        res += ((prefix_sum[right[i] + 1] - prefix_sum[i + 1]) * (i - left[i]) % MOD + MOD - 
+               (prefix_sum[i + 1] - prefix_sum[left[i] + 1]) * (right[i] - i) % MOD) % MOD * st[i] % MOD;
+        res %= MOD;
+    }
+    return (int) res;
+}
+};
